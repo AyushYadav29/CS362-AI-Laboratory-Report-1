4b3316e99dd71b53b154e8e7918d65d9876aecf8
Ayush Yadav <56931013+AyushYadav29@users.noreply.github.com>
2021-04-27 00:37:05 +0530

    Add files via upload

---
+++ Week_6/Q2.m
@@ -0,0 +1,23 @@
+clear all;
+close all;
+
+y=csvread("2020_ten_bent_coins.csv");
+
+theta = [.2 .5 .1 .3 .4 .6 .7 .8 .9 .2];
+
+h = sum(y,2); t = 100-h;
+th(1,:) = theta;
+
+for n=2:500
+ LH=zeros(size(h));
+ for k=1:10
+  lh(:,k) = (th(n-1,k).^h).*((1-th(n-1,k)).^t);
+  LH=LH+lh(:,k);
+ end
+ for k=1:10
+  th(n,k)=sum(h.*lh(:,k)./LH)/sum((h+t).*lh(:,k)./LH);
+ end
+end
+
+display('Unknown bias values are:');
+display(th);
